b867809e5626b8112c2dd0309e7e388b2a51f34b
Felipe Apostol <flipjs.io@gmail.com>
2016-10-15 11:11:34 +0100

    update hyper config

--- .hyper.js
+++ .hyper.js
@@ -81,7 +81,11 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [],
+  plugins: [
+    "hyperline",
+    "hypercwd",
+    "hyperterm-tab-icons"
+  ],
 
   // in development, you can create a directory under
   // `~/.hyperterm_plugins/local/` and include it here
